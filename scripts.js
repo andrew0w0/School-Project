06ab26e879c5c885004af5f0d182461eb2988b9a
andrew0w0 <90263133+andrew0w0@users.noreply.github.com>
2023-11-13 20:30:06 +0100

    Some changes
    FINITO IL SITO SIUM

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
-window.onload = function () {
+
+
+window.onload = async function () {
+  const div = document.getElementById('explanation');
+
   const directories = [
     { directory: 'sudamerica', centertext: 'SUD AMERICA', images: [2, 3, 4, 5, 6, 7] },
-    { directory: 'brasile', centertext: 'BRASILE', images: [8, 9, 10, 11, 12, 13, 14] }
+    { directory: 'brasile', centertext: 'BRASILE', images: [8, 9, 10, 11, 12, 13, 14] },
+    { directory: 'argentina', centertext: 'ARGENTINA', images: [21, 22, 23, 24, 25, 26, 27] },
+    { directory: 'venezuela', centertext: 'VENEZUELA', images: [36, 37, 38, 39, 40] },
+    { directory: 'peru', centertext: 'PERÙ', images: [15, 16, 17, 18, 19, 20] },
+    { directory: 'colombia', centertext: 'COLOMBIA', images: [29, 30, 31, 32, 33, 34] }
    ]
    
    const center_text = document.getElementsByClassName('center-text').item(0).innerHTML;
@@ -9,10 +17,11 @@ window.onload = function () {
    const directory = directories.find(element => element.centertext === center_text);
    console.log('center_text', center_text)
    console.log('directory', directory)
-   const div = document.getElementById('explanation');
    
    for (const image of directory.images) {
      var imageElement = document.createElement('img')
+
+     if(directory.images.indexOf(image) == 0) imageElement.style.paddingTop = '0px'
 
      if(center_text == 'SUD AMERICA') imageElement.src = `./src/images/${directory.directory}/${image}.png`;
      else imageElement.src = `../../images/${directory.directory}/${image}.png`;
@@ -21,4 +30,16 @@ window.onload = function () {
    
      div.appendChild(imageElement);
    }
+
+   /*div.addEventListener('', function() {
+    // Codice da eseguire quando l'utente entra nel divisore
+  document.getElementsByTagName('header').item(0).style.background = "rgba(0, 0, 0, 0.8)";
+    // Puoi aggiungere qui qualsiasi altra logica o azione che desideri eseguire
+});
+
+div.addEventListener('focusout', function() {
+  // Codice da eseguire quando l'utente entra nel divisore
+document.getElementsByTagName('header').item(0).style.background = "rgba(0, 0, 0, 0.2)";
+  // Puoi aggiungere qui qualsiasi altra logica o azione che desideri eseguire
+})*/
 }
